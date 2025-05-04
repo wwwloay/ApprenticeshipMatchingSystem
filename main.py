270031f189f2ca9f5e76258f3885a5b42d64b0ba
wwwloay <greenplyer@gmail.com>
2025-05-04 19:56:58 +0300

    fixed gpa ,number

--- main.py
+++ main.py
@@ -199,6 +199,26 @@ class LoginWindow(QWidget):
             QMessageBox.warning(self, "Error", "Please fill in all fields.")
             return
 
+        if self.user_type == "Student":
+            try:
+                gpa = float(data["GPA"])
+                if gpa < 0 or gpa > 5:
+                    raise ValueError
+            except ValueError:
+                QMessageBox.warning(self, "Invalid GPA", "GPA must be a number between 0 and 5.")
+                return
+        
+        phone_number = data["MobileNumber"]
+        if not phone_number.isdigit() or len(phone_number) != 10 or not phone_number.startswith("05"):
+            QMessageBox.warning(self, "Invalid Phone Number", "Phone number must be exactly 10 digits and start with '05'.")
+            return
+
+        student_id = data["StudentId"]
+        if not student_id.isdigit() or len(student_id) != 7 or not (1500000 <= int(student_id) <= 2699999):
+            QMessageBox.warning(self, "Invalid Student ID", "Student ID must be exactly 7 digits and between 1500000 and 2699999.")
+            return
+
+
         if self.user_type == "Company":
             company = Company(
                 CompanyId=None,
